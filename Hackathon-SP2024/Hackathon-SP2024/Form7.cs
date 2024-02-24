03e01c59f12b73efc01268a596970ddd9a351ab7
Julia Johnson <100613151+JuliaCJ@users.noreply.github.com>
2024-02-24 09:30:39 -0500

    Add files via upload

---
+++ Hackathon-SP2024/Hackathon-SP2024/Form7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hackathon_SP2024
+{
+    public partial class Form7 : Form
+    {
+        AccountManager man;
+        public Form7(AccountManager man)
+        {
+            InitializeComponent();
+            this.man = man;
+        }
+
+
+        private void CreateAccount_Click_1(object sender, EventArgs e)
+        {
+            man.addPerson(NewName.Text, NewUser.Text, NewPass.Text, NewEmail.Text, NewPhone.Text, NewGender.Text, BdayBox.Text);
+            man.saveAccounts();
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+
+        }
+    }
+}
---
+++ Hackathon-SP2024/Hackathon-SP2024/Helper.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon_SP2024
+{
+    public class Helper : Account
+    {
+        string des;
+        string hours;
+        int cap;
+        int current;
+        float percentFull;
+        string limits;
+        string address;
+
+        public Helper(string user, string pass, string name, string email, string phone, string des, string hours, int cap, int current, string limits, string address) : base(user, pass, name, email, phone)
+        {
+            this.des = des;
+            this.cap = cap;
+            this.hours = hours;
+            this.cap = cap;
+            this.current = current;
+            this.limits = limits;
+            this.address = address;
+
+            percentFull = ((float)current / (float)cap) * 100;
+        }
+
+        public string getDes()
+        {
+            return des;
+        }
+
+        public string getHours()
+        {
+            return hours;
+        }
+
+        public int getCap()
+        {
+            return cap;
+        }
+
+        public int getCurrent()
+        {
+            return current;
+        }
+
+        public float getPercentFull()
+        {
+            return percentFull;
+        }
+        public string getLimits()
+        {
+            return limits;
+        }
+
+        public string getAddress()
+        {
+            return address;
+        }
+
+        public override string getType()
+        {
+            return "Helper";
+        }
+        public void setDes(string Des)  
+        {
+            this.des = Des;
+        }
+
+        public void setHours(string hours)
+        {
+         this.hours= hours;
+        }
+
+        public void setCap(int cap)
+        {
+            this.cap = cap;
+        }
+
+        public void setCurrent(int current)
+        {
+           this.current= current;
+            percentFull= ((float)current / (float)cap)*100;
+        }
+
+        public void setLimits(string limits)
+        {
+          this.limits = limits;
+        }
+
+        public void setAddress(string adrs)
+        {
+            this.address = adrs;
+        }
+
+        public string prettyDes()
+        {
+            string[] line = this.des.Split(';');
+            string newDes = "";
+     
+
+            foreach (string s in line)
+            {
+              
+                newDes += s + "\n";
+             
+            }
+            return newDes;
+        }
+
+        public string prettyAdrs()
+        {
+            string[] line = this.address.Split(';');
+            string adrs = "";
+            bool first = true;
+
+            foreach (string s in line)
+            {
+        
+                adrs += s + "\n";
+             
+            }
+            
+            return adrs;
+        }
+
+        public override string OtherInfo()
+        {
+            return "-,-," + this.des + "," + this.hours + "," + this.cap + "," + this.current + "," + this.limits + "," + this.address;
+        }
+
+        public override string ToString()
+        {
+            return this.name + "\n" + prettyAdrs()  + this.limits + "\n" + this.hours + "\n" + "This location is " + this.percentFull + "% full.";
+        }
+
+    }
+}
